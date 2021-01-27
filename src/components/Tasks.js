9409c188d468006a7da1b6189e9afaab140a249c
Harry Jiao <47682425+MindlessCode@users.noreply.github.com>
2021-01-26 21:25:30 -0500

    c

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,8 @@
 import Task from './Task'
+import moment from 'moment'
 import './Task.css';
 
 const Tasks =({tasks, startDate, onDelete})=> {
-
     return (
         <div className="taskContainer">
           <h2> Date: {startDate.sDate}</h2>
@@ -10,6 +10,7 @@ const Tasks =({tasks, startDate, onDelete})=> {
                 <Task key={task.id} task={task} startDate={startDate} onDelete={onDelete}/>
             )
           )}
+          {(startDate.sDate >= moment().format("YYYY-MM-DD")) ? <input type="button" value="Submit"className="subBtn"></input> : <input type="button" value="Submit" className="subBtn" disabled></input>}
         </div>
     )
 }
